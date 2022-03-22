792cbd906786cb2eacd1f7373d47244a6cfdd76f
adriencances <adriencances@gmail.com>
2022-03-22 23:27:31 +0100

    avancée du notebook

--- mstamp/bits_compression.py
+++ mstamp/bits_compression.py
@@ -27,7 +27,7 @@ def DL(T):
         DL score of T
     """
     m, dim = T.shape
-    return T.nbytes/dim
+    return T.nbytes*8
 
 def RDL(t_c, t_h):
     """compute RDL of t_c according to t_h
@@ -44,15 +44,15 @@ def RDL(t_c, t_h):
     diff = t_c - t_h
     gamma = np.sum(diff != 0)
     m, dim = t_c.shape
-    b = int(t_c.nbytes/(m*dim))
+    b = int(t_c.nbytes/(m*dim))*8
+    # b = int(t_c.nbytes/(m*dim))
     return gamma*(np.log2(m) + b)
-    # return gamma*(np.log2(m) + b)/dim
 
-def bit(multivariate_time_series, compressible_set_index, hypothesis_index, sub_len, unexplored_index=None):
+def bit(time_series, compressible_set_index, hypothesis_index, sub_len, unexplored_index=None):
     """Compute the bits required to store compressible_set_index according to hypothesis_index
 
     Parameters:
-        multivariate_time_series: list of index
+        time_series: list of index
         compressible_set_index: list of index
         hypothesis_index: list of index
         sub_len: int
@@ -64,9 +64,9 @@ def bit(multivariate_time_series, compressible_set_index, hypothesis_index, sub_
     compressible_set = []
     hypothesis_set = []
     for idx, prof in compressible_set_index:
-        compressible_set.append(Dnorm(multivariate_time_series[idx: idx + sub_len, prof]))
+        compressible_set.append(Dnorm(time_series[idx: idx + sub_len, prof]))
     for idx, prof in hypothesis_index:
-        hypothesis_set.append(Dnorm(multivariate_time_series[idx: idx + sub_len, prof]))
+        hypothesis_set.append(Dnorm(time_series[idx: idx + sub_len, prof]))
     h = len(hypothesis_set)
     res = 0
     for t_c in compressible_set:
@@ -83,24 +83,24 @@ def bit(multivariate_time_series, compressible_set_index, hypothesis_index, sub_
             res += DL(t_u)
     return res
 
-def MDL(matrix_profile, multivariate_time_series, sub_len, profile):
+def MDL(matrix_profile, time_series, sub_len, profile):
     """mdl methods, compute the bits that are needed to store the
     two subsequences selected for each dimension
 
     Parameters:
         matrix_profil: np.array (n_dim, m)
-        multivariate_time_series: np.array (m, n_dim)
+        time_series: np.array (m, n_dim)
         sub_len: int
         profile: list of int (n_dim lists)
 
     Return:
         compression value for each dimension computed with RDL
     """
-    dimension, t = matrix_profile.shape
+    dimension = matrix_profile.shape[0]
     bits = []
     for d in range(dimension):
         t_1, t_2 = matrix_profile[d, :].argsort()[:2]
         hypothesis_index = [(t_1, profile[d][:,t_1])]
         compressible_set_index = [(t_2, profile[d][:,t_2])]
-        bits.append(bit(multivariate_time_series, compressible_set_index, hypothesis_index, sub_len))
+        bits.append(bit(time_series, compressible_set_index, hypothesis_index, sub_len))
     return np.array(bits)
